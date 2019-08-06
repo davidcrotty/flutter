42c61c35eb89cd2898007ac650baaa4d233d33bc
David Crotty <david.crotty.work@gmail.com>
2019-08-06 22:06:33 +0100

    provider accurate location

--- splitcalculator/split_calculator/lib/CalculatorForm.dart
+++ splitcalculator/split_calculator/lib/CalculatorForm.dart
@@ -22,145 +22,151 @@ class CalculatorInputForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Hero(
-          tag: "table",
-          child: Container(
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(16)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Expanded(
-                  child: Column(
-                    children: <Widget>[
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: Container(
-                          height: 80,
-                          child: ButtonTheme(
-                            height: 36,
-                            minWidth: 36,
-                            child: FlatButton(
-                              shape: CircleBorder(),
-                              onPressed: () {
-                                print("Pressed");
-                              },
-                              child: Icon(
-                                Icons.keyboard_arrow_down,
-                                color: Colors.grey,
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return Stack(
+          children: <Widget>[
+            Hero(
+              tag: "table",
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Expanded(
+                      child: Column(
+                        children: <Widget>[
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: Container(
+                              height: 80,
+                              child: ButtonTheme(
+                                height: 36,
+                                minWidth: 36,
+                                child: FlatButton(
+                                  shape: CircleBorder(),
+                                  onPressed: () {
+                                    print("Pressed");
+                                  },
+                                  child: Icon(
+                                    Icons.keyboard_arrow_down,
+                                    color: Colors.grey,
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                      Expanded(
-                          child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: <Widget>[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
+                          Expanded(
+                              child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.end,
                               children: <Widget>[
-                                Flexible(
-                                  flex: 1,
-                                  child: ButtonTheme(
-                                    height: 36,
-                                    minWidth: 36,
-                                    child: FlatButton(
-                                      shape: CircleBorder(),
-                                      onPressed: () {
-                                        print("Tapped");
-                                      },
-                                      child: Icon(
-                                        Icons.clear,
-                                        color: Colors.grey,
-                                        size: 16,
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: <Widget>[
+                                    Flexible(
+                                      flex: 1,
+                                      child: ButtonTheme(
+                                        height: 36,
+                                        minWidth: 36,
+                                        child: FlatButton(
+                                          shape: CircleBorder(),
+                                          onPressed: () {
+                                            print("Tapped");
+                                          },
+                                          child: Icon(
+                                            Icons.clear,
+                                            color: Colors.grey,
+                                            size: 16,
+                                          ),
+                                        ),
                                       ),
                                     ),
-                                  ),
+                                    Flexible(
+                                      flex: 1,
+                                      child: Container(),
+                                    )
+                                  ],
                                 ),
-                                Flexible(
-                                  flex: 1,
-                                  child: Container(),
+                                Align(
+                                  child: Text("Value",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.grey)),
+                                  alignment: Alignment.centerRight,
                                 )
                               ],
                             ),
-                            Align(
-                              child: Text("Value",
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.grey)),
-                              alignment: Alignment.centerRight,
-                            )
-                          ],
-                        ),
-                      )),
-                      Container(
-                        decoration: BoxDecoration(
-                            border: Border(
-                                top: BorderSide(
-                                    color: Colors.grey, width: 0.5))),
-                        height: 80,
-                        child: Material(
-                          borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(16),
-                              bottomRight: Radius.circular(16)),
-                          child: InkWell(
-                            borderRadius: BorderRadius.only(
-                                bottomLeft: Radius.circular(16),
-                                bottomRight: Radius.circular(16)),
-                            child: Align(
-                              alignment: Alignment.center,
-                              child: Text(
-                                "Calculate",
-                                style: TextStyle(
-                                    color: Colors.purple,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold),
+                          )),
+                          Container(
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    top: BorderSide(
+                                        color: Colors.grey, width: 0.5))),
+                            height: 80,
+                            child: Material(
+                              borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(16),
+                                  bottomRight: Radius.circular(16)),
+                              child: InkWell(
+                                borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(16),
+                                    bottomRight: Radius.circular(16)),
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "Calculate",
+                                    style: TextStyle(
+                                        color: Colors.purple,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                onTap: () {
+                                  print("tapped");
+                                },
                               ),
                             ),
-                            onTap: () {
-                              print("tapped");
-                            },
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
-        Positioned( // wrap this in builder
-            right: 16,
-            top: 272,
-            child: Hero(
-                tag: "text",
-                flightShuttleBuilder: (BuildContext flightContext,
-                    Animation<double> animation,
-                    HeroFlightDirection flightDirection,
-                    BuildContext fromHeroContext,
-                    BuildContext toHeroContext) {
-                  return toHeroContext.widget;
-                },
-                child: FittedBox(
-                  child: Material(
-                      type: MaterialType.transparency,
-                      child: Container(
-                          child: Text("220,45",
-                              style: TextStyle(
-                                  fontSize: 50,
-                                  color: Colors.purple,
-                                  backgroundColor: Colors.white,
-                                  fontWeight: FontWeight.bold)))),
-                )))
-      ],
+            Positioned(
+                // wrap this in builder
+                right: 16,
+                top: (constraints.maxHeight / 2) - 50,
+                child: Hero(
+                    tag: "text",
+                    flightShuttleBuilder: (BuildContext flightContext,
+                        Animation<double> animation,
+                        HeroFlightDirection flightDirection,
+                        BuildContext fromHeroContext,
+                        BuildContext toHeroContext) {
+                      return toHeroContext.widget;
+                    },
+                    child: FittedBox(
+                      child: Material(
+                          type: MaterialType.transparency,
+                          child: Container(
+                              child: Text("220,45",
+                                  style: TextStyle(
+                                      fontSize: 50,
+                                      color: Colors.purple,
+                                      backgroundColor: Colors.white,
+                                      fontWeight: FontWeight.bold)))),
+                    )))
+          ],
+        );
+      },
     );
   }
 }
